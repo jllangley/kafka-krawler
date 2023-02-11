d5c42f7ba8407c74ca4272e41b9bc3cc68aa854d
jllangley <orig3n@localhost.com>
2023-02-11 08:44:54 -0600

    First

---
+++ README.md
@@ -0,0 +1,17 @@
+# Kafka-Krawler
+
+## This script was designed to iterate through all kafka topics on a server and download the data in each topic. Data will be saved as json files in a {ip_address}.{topic_name}.json format.
+
+### Usage:
+- usage for one ip address.
+```bash
+python3 kafka-krawler.py --bootstrap-server {ip_address}
+```
+- usage for multiple ip addresses
+  - Put your list of servers to in file "bootstrap_servers.txt"
+  - one server per line.
+  ```bash
+  python3 kafka-krawler.py
+  ```
+
+  
---
+++ kafka-krawler.py
@@ -0,0 +1,37 @@
+import json
+import traceback
+from concurrent.futures import ThreadPoolExecutor
+from kafka import KafkaConsumer
+
+# Read the list of bootstrap servers from the input file
+with open('bootstrap_servers.txt', 'r') as f:
+    bootstrap_servers = f.read().splitlines()
+
+def process_server_topics(server):
+    # Create a Kafka consumer for the current server
+    try:
+        consumer = KafkaConsumer(bootstrap_servers=server, auto_offset_reset='earliest')
+
+        # Get a list of all topics for the current server
+        topics = consumer.topics()
+
+        # Iterate through each topic
+        for topic in topics:
+            print(server + " - " + topic)
+            consumer.subscribe(topics=topic)
+            # Write messages from the current topic to a JSON file named after the server and topic
+            messages = consumer.poll(timeout_ms=1000).values()
+
+            if messages:
+                with open(f'{server}_{topic}.json', 'w', encoding='utf-8') as f:
+                    for message in messages:
+                        for record in message:
+                            value = json.loads(record.value)
+                            if value:
+                                f.write(json.dumps(value, indent=4) + '\n')
+    except Exception:
+        traceback.print_exc()
+
+# Use a ThreadPoolExecutor to run multiple instances of the for loop concurrently
+with ThreadPoolExecutor() as executor:
+    executor.map(process_server_topics, bootstrap_servers)
